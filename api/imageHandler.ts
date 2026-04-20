741cdb92626568add05eaf55bd4fe4edf07a35a1
Shobhan Patra <230170146055@vgecg.ac.in>
2026-04-20 20:56:30 +0530

    Refactor image handling

--- api/imageHandler.ts
+++ api/imageHandler.ts
@@ -6,6 +6,8 @@ import {ApiError} from "../utils/apiError.ts";
 import {db} from "../db/db.ts";
 import imageProcessingJobQueue from "../queue/queue.ts";
 import {uuid as v4} from "uuidv4";
+import path from "path";
+import * as fs from "node:fs";
 const bucketName = process.env.BUCKET_NAME!;
 
 // Creates a client
@@ -14,25 +16,23 @@ const storage = new Storage({
     keyFilename: process.env.BUCKET_KEY_FILENAME!,
 });
 
-async function getSignedDownloadURL(imageHash: string) {
+async function getSignedDownloadURL(imageHash: string, fileExt: string) {
     const options = {
         version: 'v4',
         action: 'read',
-        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+        expires: Date.now() + 30 * 60 * 1000, // 30 minutes
     };
 
     // Get a v4 signed URL for reading the file
-    const url = await storage.bucket(bucketName).file(imageHash).getSignedUrl(options);
-
-    console.log('Generated GET signed URL:');
-    console.log(url[0]);
+    const fileName = imageHash + fileExt;
+    const url = await storage.bucket(bucketName).file(fileName).getSignedUrl(options);
 
     return url[0];
 }
 
 async function uploadFile(fileName: string) {
     const filePath = `uploads/${fileName}`;
-    const destinationFileName = await getHash(filePath); // hash the image file to reduce redundant checks later
+    const destinationFileName = await getHash(filePath) + path.extname(filePath); // hash the image file to reduce redundant checks later
 
     const options = {
         destination: destinationFileName!,
@@ -43,47 +43,61 @@ async function uploadFile(fileName: string) {
 }
 
 const uploadImage = async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.file?.filename) {
-        throw new ApiError(404, `No file provided`);
+    const file = req.file;
+    if (!file?.filename) {
+        return next(new ApiError(400, "No file provided"));
     }
 
     try {
-        // update image metadata in DB
-        const imageHash = await getHash('uploads/' + req.file.filename) || "";
+        // Compute image hash
+        const localImagePath = path.join('uploads', file.filename);
+        const imageHash = await getHash(localImagePath) || "";
+        const extName = path.extname(file.filename);
 
-        const existingImage = await db.execute('SELECT * FROM images WHERE hash_value = ?', [imageHash]);
+        const { rows } = await db.execute('SELECT id, caption FROM images WHERE hash_value = ?', [imageHash]);
+        const existingImage = rows[0];
 
-        if (existingImage.rows.length > 0) {
-
-            if (!existingImage.rows[0]?.caption) {
-                return res
-                    .status(202)
-                    .json(new ApiResponse(202, null, "Image is being processed. Retry later"));
-            }
-
-            return res
-                .status(200)
-                .json(new ApiResponse(200, {caption: existingImage.rows[0]?.caption},
+        // Image already processed
+        if (existingImage?.caption) {
+            return res.status(200)
+                .json(new ApiResponse(200, {
+                        caption: existingImage.caption
+                        },
                     "Retrieved from DB (Previously processed image)"
-                )
+                    )
+                );
+        }
+
+        // Image is being processed
+        const existingJob = await imageProcessingJobQueue.getJob(imageHash);
+        if (existingImage || existingJob) {
+            return res.status(202).json(
+                new ApiResponse(202, { imageHash: imageHash, jobId: imageHash }, "Image is already in the processing queue")
             );
         }
 
-        // upload file to GCS
-        await uploadFile(req.file.filename);
+        // New Image
+        await uploadFile(req.file.filename); // upload file to GCS
 
         const imageId = v4();
-        const imageUrl = await getSignedDownloadURL(imageHash);
+        const imageUrl = await getSignedDownloadURL(imageHash, path.extname(file.filename));
 
         // update DB
         await db.execute(`INSERT INTO images (id, hash_value, url) VALUES (?, ?, ?); `, [imageId, imageHash, imageUrl]);
 
-        await imageProcessingJobQueue.add(imageHash, {
-            image: req.file,
-        });
-        console.log(`${imageHash} (${req.file.filename}) is sent to processing queue`);
+        const job = await imageProcessingJobQueue.add("captioning",
+            { image: file, imageUrl, imageHash },
+            { jobId: imageHash } // To prevent duplicate jobs
+        );
+        console.log(`${imageHash} (${file.filename}) is sent to processing queue`);
 
-        return res.status(200).json(new ApiResponse(200, imageHash, "Image is being processed"));
+        fs.unlinkSync(localImagePath); // delete local image
+
+        return res.status(202).json(new ApiResponse(200, {
+            imageHash: imageHash,
+            jobId: job.id
+        }, "Image sent to processing queue"));
+
     } catch (error) {
         console.log(error);
         next(error);
