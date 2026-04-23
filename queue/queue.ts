cbc97dadc5694b082ab881e3a30c4ebe4e0886c2
Shobhan Patra <230170146055@vgecg.ac.in>
2026-04-23 17:42:11 +0530

    Add bullmq limiter

--- queue/queue.ts
+++ queue/queue.ts
@@ -1,6 +1,14 @@
 import { Queue } from "bullmq";
 import {connection} from "../config/queueConnection.ts";
 
-const imageProcessingJobQueue = new Queue("image-processing-jobs", { connection });
+const imageProcessingJobQueue = new Queue("image-processing-jobs",
+    {
+        connection,
+        limiter: {
+            max: 10,
+            duration: 1000
+        }
+    }
+);
 
 export default imageProcessingJobQueue;
